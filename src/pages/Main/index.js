4e41055b3b1199a7a72f545341c968c5c096c866
Tiago Lima <tiagolima@Tiagos-Mac-mini.local>
2019-07-01 18:15:23 -0300

    Application Completed.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
 import { 
     Container, 
@@ -17,12 +19,63 @@ import Tabs from '../../components/Tabs';
 import Menu from '../../components/Menu';
 
 export default function Main() {
+    let offset = 0;
+    const translateY = new Animated.Value(0);
+    
+    const animatedEvent = Animated.event(
+        [
+          {
+            nativeEvent: {
+              translationY: translateY,
+            },
+          },
+        ],
+        { useNativeDriver: true },
+    );
+
+    function onHandlerStateChanged(event) {
+        if (event.nativeEvent.oldState === State.ACTIVE) {
+          let opened = false;
+          const { translationY } = event.nativeEvent;
+    
+          offset += translationY;
+    
+          if (translationY >= 100) {
+            opened = true;
+          } else {
+            translateY.setValue(offset);
+            translateY.setOffset(0);
+            offset = 0;
+          }
+    
+          Animated.timing(translateY, {
+            toValue: opened ? 380 : 0,
+            duration: 200,
+            useNativeDriver: true,
+          }).start(() => {
+            offset = opened ? 380 : 0;
+            translateY.setOffset(offset);
+            translateY.setValue(0);
+          });
+        }
+      }
+
     return (
         <Container>
             <Header />
             <Content>
-                <Menu />
-                <Card>
+                <Menu translateY={translateY} />
+                <PanGestureHandler
+                    onGestureEvent={animatedEvent}
+                    onHandlerStateChange={onHandlerStateChanged} >
+                <Card style={{
+                    transform:[{
+                            translateY: translateY.interpolate({
+                                inputRange: [-350, 0, 380],
+                                outputRange: [-50, 0, 380],
+                                extrapolate: 'clamp'
+                            }),
+                        }]}}>
                     <CardHeader>
                         <Icon name="attach-money" size={28} color="#666" />
                         <Icon name="visibility-off" size={28} color="#666" />
@@ -37,8 +90,9 @@ export default function Main() {
                         </Annotation>
                     </CardFooter>
                 </Card>
+                </PanGestureHandler>
             </Content>
-            <Tabs />
+            <Tabs translateY={translateY} />
         </Container>
     )
 }
